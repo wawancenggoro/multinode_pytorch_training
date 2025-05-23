0ada1c5769ea7f8c649a9cc131f6a529c85bcc85
Wawan Cenggoro <wawancenggoro@gmail.com>
2025-05-23 09:38:03 +0000

    revise test.sh

--- DDP/test.sh
+++ DDP/test.sh
@@ -6,17 +6,17 @@
 ### e.g. request 2 nodes with 1 gpu each, totally 2 gpus (WORLD_SIZE==2)
 ### Note: --gres=gpu:x should equal to ntasks-per-node
 #SBATCH --nodes=3
-#SBATCH --ntasks-per-node=2
-#SBATCH --gres=gpu:2
+#SBATCH --ntasks-per-node=8
+#SBATCH --gres=gpu:8
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=1792
+#SBATCH --mem=1792G
 #SBATCH --chdir=[PATH_TO_THE_MAIN_FOLDER]
 #SBATCH --output=slurm_monitoring/%x-%j.out
 
 ### change 5-digit MASTER_PORT as you wish, slurm will raise Error if duplicated with others
 ### change WORLD_SIZE as gpus/node * num_nodes
 export MASTER_PORT=9304
-export WORLD_SIZE=6
+export WORLD_SIZE=24
 
 ### get the first node name as master address - customized for vgg slurm
 ### e.g. master(gnodee[2-5],gnoded1) == gnodee2
